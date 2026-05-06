abf1c8d09ce23e407e7f90748892003478dc5833
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-05-06 14:02:32 -0400

    created formatted version

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -8,4 +8,8 @@ public class NameFormatter {
     public static String format(String firstName, String lastName){
         return lastName + "," + firstName;
     }
+
+    public static String format(String prefix, String firstName, String middleName, String lastName, String suffix ){
+
+    }
 }
