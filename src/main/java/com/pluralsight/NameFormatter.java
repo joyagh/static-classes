8474fe7c3776544914ad6af615393937af036195
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-05-06 14:11:21 -0400

    added firstName to formatter

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -10,8 +10,15 @@ public class NameFormatter {
     }
 
     public static String format(String prefix, String firstName, String middleName, String lastName, String suffix ){
-        return prefix + middleName + lastName + suffix;
+        return prefix + firstName + middleName + lastName + suffix;
 
 
+    }
+    public static String format(String fullName){
+        String[] name = fullName.split(" ");
+        if (name.length == 5){
+            String prefix = name[0];
+            Stri
+        }
     }
 }
